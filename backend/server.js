3e34c8f5f4bf8a52b11fea72c9c08710293670f7
ubbagg <garvgarg1902@gmail.com>
2025-07-20 08:13:59 +0530

    Main push 5

--- backend/server.js
+++ backend/server.js
@@ -106,7 +106,7 @@ app.use('/*path', (req, res) => {
 });
 
 // Handle preflight requests explicitly
-app.options('*', (req, res) => {
+app.options('/*path', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'https://my-kitchen-buddy-hnb7.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
